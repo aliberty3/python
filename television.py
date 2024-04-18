4df4d77501fc6c403ca1cf2bb451793b09d43d5c
aliberty3 <122935292+aliberty3@users.noreply.github.com>
2024-04-18 18:57:57 -0500

    First
    End doesn't print right for now

---
+++ television.py
@@ -0,0 +1,60 @@
+class Television:
+    # Class variables
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        # Instance variables
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+    def power(self):
+        # Method to turn the TV on and off
+        self.__status = not self.__status
+
+    def mute(self):
+        # Method to mute and unmute the TV
+        if self.__status:
+            self.__muted = not self.__muted
+            if self.__muted:
+                self.__volume = self.MIN_VOLUME  # Muting sets volume to minimum
+
+    def channel_up(self):
+        # Method to increase TV channel
+        if self.__status:
+            self.__channel = (self.__channel + 1) % (self.MAX_CHANNEL + 1)
+
+    def channel_down(self):
+        # Method to decrease TV channel
+        if self.__status:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
+
+    def volume_up(self):
+        # Method to increase TV volume
+        if self.__status:
+            if self.__muted:
+                self.__muted = False  # Unmute if volume is adjusted while muted
+            if self.__volume < self.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self):
+        # Method to decrease TV volume
+        if self.__status:
+            if self.__muted:
+                self.__muted = False  # Unmute if volume is adjusted while muted
+            if self.__volume > self.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self):
+        # Method to return the state of the TV
+        return f"Power [{self.__status}], Channel [{self.__channel}], Volume [{self.__volume}]"
+
+
+
