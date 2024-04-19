661439b02d1db0f24c2be78783898ce5e4f56461
aliberty3 <122935292+aliberty3@users.noreply.github.com>
2024-04-18 21:00:01 -0500

    Second Push
    Fourth line output

--- television.py
+++ television.py
@@ -11,6 +11,7 @@ class Television:
         self.__muted = False
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
+        self.__previous_volume = self.MIN_VOLUME  # Initialize previous volume
 
     def power(self):
         # Method to turn the TV on and off
@@ -19,9 +20,13 @@ class Television:
     def mute(self):
         # Method to mute and unmute the TV
         if self.__status:
+            if not self.__muted:  # Only store previous volume if TV was not already muted
+                self.__previous_volume = self.__volume
             self.__muted = not self.__muted
             if self.__muted:
                 self.__volume = self.MIN_VOLUME  # Muting sets volume to minimum
+            elif not self.__muted:
+                self.__volume = self.__previous_volume  # Unmute sets volume to previous level
 
     def channel_up(self):
         # Method to increase TV channel
@@ -41,7 +46,8 @@ class Television:
         if self.__status:
             if self.__muted:
                 self.__muted = False  # Unmute if volume is adjusted while muted
-            if self.__volume < self.MAX_VOLUME:
+                self.__volume = self.__previous_volume + 1 if self.__previous_volume < self.MAX_VOLUME else self.MAX_VOLUME  # Set volume to minimum non-muted level
+            elif self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
@@ -49,12 +55,11 @@ class Television:
         if self.__status:
             if self.__muted:
                 self.__muted = False  # Unmute if volume is adjusted while muted
-            if self.__volume > self.MIN_VOLUME:
+                self.__volume = self.__previous_volume + 1 if self.__previous_volume < self.MAX_VOLUME else self.MAX_VOLUME  # Set volume to minimum non-muted level
+            elif self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
 
     def __str__(self):
         # Method to return the state of the TV
         return f"Power [{self.__status}], Channel [{self.__channel}], Volume [{self.__volume}]"
-
-
 
